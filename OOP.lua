9949bbae8f7cef5ceb55bc2b4f0c4e2ddbc0e3cc
Porsche Sueanok <nsuean2@uic.edu>
2025-12-02 02:31:11 -0500

    Add OOP example with Animal and Dog classes
    This is the example of OOP code with inheritsnce included

--- OOP.lua
+++ OOP.lua
@@ -1,1 +1,20 @@
--- TODO: make OOP example (preferably with inheritance)
+Animal = {}
+
+function Animal:speak()
+    print("Animal makes a sound")
+end
+
+
+Dog = { __index = Dog }
+setmetatable(Dog, { __index = Animal })
+
+function Dog:speak() 
+    print("Bark")
+end
+
+
+animal = setmetatable({}, Animal)
+dog = setmetatable({}, Dog)
+
+animal:speak()  
+dog:speak()  
